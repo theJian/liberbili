2216fc043c4d690e5127e35c7c192c341fb6ad6a
Jiàn <hi@chabuduo.in>
2026-07-21 16:41:21 +0800

    fix: hide native tab labels

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,22 +1,20 @@
 import { NativeTabs } from 'expo-router/unstable-native-tabs';
-import { useLingui } from '@lingui/react/macro';
 
 import { MiniPlayer } from '@/components/mini-player';
 
 export default function TabsLayout() {
-  const { t } = useLingui();
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
-        <NativeTabs.Trigger.Label>{t`Home`}</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Label hidden />
         <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="playlists">
-        <NativeTabs.Trigger.Label>{t`Playlists`}</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Label hidden />
         <NativeTabs.Trigger.Icon sf="music.note.list" md="queue_music" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="downloads">
-        <NativeTabs.Trigger.Label>{t`Downloads`}</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Label hidden />
         <NativeTabs.Trigger.Icon sf="arrow.down.circle.fill" md="download" />
       </NativeTabs.Trigger>
       <NativeTabs.BottomAccessory><MiniPlayer /></NativeTabs.BottomAccessory>
